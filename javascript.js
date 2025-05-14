ae996f206084092f5baf8509377000e6322ab327
Yudensei <yudensei.140@hotmail.com>
2025-05-14 15:59:59 -0300

    Write getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!")
+function getComputerChoice() {
+    let rng = Math.floor(Math.random() * 3)
+    if (rng == 0) {
+        return "rock"
+    } else if (rng == 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
