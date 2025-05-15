c4870d2d92a98d6afe45cffb178639a87acd96b9
Yudensei <yudensei.140@hotmail.com>
2025-05-15 06:29:05 -0300

    Make UI with HTML

--- javascript.js
+++ javascript.js
@@ -1,41 +1,59 @@
-playGame();
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+const choiceButtons = document.querySelector("#choiceButtons");
+const playButton = document.querySelector("#playButton");
 
-  playRound(getHumanChoice(), 0)
+const score = document.querySelector("#scores")
+const numHumanScore = document.querySelector("#humanScore span");
+const numComputerScore = document.querySelector("#computerScore span")
 
-  if (humanScore > computerScore) {
-    console.log("You win!");
-  } else if (humanScore < computerScore) {
-    console.log("You lose!");
+const resultMessage = document.querySelector("#result");
+
+playButton.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  numHumanScore.textContent = humanScore;
+  numComputerScore.textContent = computerScore;
+  playButton.style.display = "none";
+  choiceButtons.style.display = "block";
+});
+
+choiceButtons.addEventListener("click", (e) => {
+  playRound(e.target.id, getComputerChoice());
+  score.style.display = "block";
+});
+
+function playRound(humanChoice, computerChoice) {
+  let choices = ["Scissors", "Paper", "Rock"];
+  if (humanChoice === choices.at(computerChoice - 1)) {
+    humanScore++;
+    numHumanScore.textContent = humanScore;
+    resultMessage.textContent = `You win! ${humanChoice} beats ${choices.at(computerChoice)}`
+  } else if (humanChoice === choices.at(computerChoice)) {
+    resultMessage.textContent = `It's a tie! ${humanChoice} ties with ${choices.at(computerChoice)}`
   } else {
-    console.log("It's a tie!");
+    computerScore++;
+    numComputerScore.textContent = computerScore;
+    resultMessage.textContent = `You lose! ${choices.at(computerChoice)} beats ${humanChoice}`
   }
 
-  function playRound(humanChoice, computerChoice) {
-    const captalizedHumanChoice = humanChoice.at(0).toUpperCase()
-                                + humanChoice.slice(1);
-    
-    choices = ["scissors", "paper", "rock"]
-    if (humanChoice === choices.at(computerChoice - 1)) {
-      humanScore++;
-      return "win"
-    } else if (humanChoice === choices.at(computerChoice)) {
-      return "tie"
-    } else {
-      computerScore++;
-      return "loss"
-    }
+  checkWinner();
+}
 
+function checkWinner() {
+  if (humanScore === 5) {
+    resultMessage.textContent = "You win!"
+  } else if (computerScore === 5) {
+    resultMessage.textContent = "You lose!"
+  } else {
+    return
   }
+
+  choiceButtons.style.display = "none";
+  playButton.style.display = "block"  
 }
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
-}
-
-function getHumanChoice() {
-  return prompt("Rock, Paper or Scissors?", "rock").toLowerCase();
 }
