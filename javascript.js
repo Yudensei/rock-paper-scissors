db5183c276debac2ec61ef1bb9d4523050132b41
Yudensei <yudensei.140@hotmail.com>
2025-05-15 04:37:15 -0300

    Make playRound logic nicer
    The logic before was made without arrays and it was a mess,
    so this makes it better with arrays

--- javascript.js
+++ javascript.js
@@ -4,11 +4,7 @@ function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), 0)
 
   if (humanScore > computerScore) {
     console.log("You win!");
@@ -21,49 +17,23 @@ function playGame() {
   function playRound(humanChoice, computerChoice) {
     const captalizedHumanChoice = humanChoice.at(0).toUpperCase()
                                 + humanChoice.slice(1);
-    let win;
-
-    if (captalizedHumanChoice == computerChoice) {
-      console.log(`It's a tie! ${computerChoice} ties with ${computerChoice}`);
-      return;
-    } else if (captalizedHumanChoice == "Rock" 
-            && computerChoice == "Scissors") {
-        win = true;
-    } else if (captalizedHumanChoice == "Paper"
-            && computerChoice == "Rock") {
-        win = true;
-    } else if (captalizedHumanChoice == "Scissors"
-            && computerChoice == "Paper") {
-        win = true;
-    } else if (captalizedHumanChoice == "Rock"
-            && computerChoice == "Paper") {
-        win = false;
-    } else if (captalizedHumanChoice == "Paper"
-            && computerChoice == "Scissors") {
-        win = false;
-    } else if (captalizedHumanChoice == "Scissors") {
-        win = false;
-    }
-
-    if (win) {
-      console.log(`You win! ${captalizedHumanChoice} beats ${computerChoice}`);
+    
+    choices = ["scissors", "paper", "rock"]
+    if (humanChoice === choices.at(computerChoice - 1)) {
       humanScore++;
+      return "win"
+    } else if (humanChoice === choices.at(computerChoice)) {
+      return "tie"
     } else {
-      console.log(`You lose! ${computerChoice} beats ${captalizedHumanChoice}`);
       computerScore++;
+      return "loss"
     }
+
   }
 }
 
 function getComputerChoice() {
-  let rng = Math.floor(Math.random() * 3);
-  if (rng == 0) {
-    return "Rock";
-  } else if (rng == 1) {
-    return "Paper";
-  } else {
-    return "Scissors";
-  }
+  return Math.floor(Math.random() * 3);
 }
 
 function getHumanChoice() {
